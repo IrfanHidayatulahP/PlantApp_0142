052bb1ca2b8718543927d95d10fca36dd396bef8
IrfanHidayatulahP <irfanhputra400@gmail.com>
2025-05-23 04:58:50 +0700

    fix bug

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,8 @@ import 'package:plant_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    String alamat = "";
+    await tester.pumpWidget(MyApp(alamat: alamat));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
